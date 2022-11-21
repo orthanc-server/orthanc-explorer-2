c86ff7ff4a4a8ff66426334f90117aeea97c25d0
Alain Mazy <alain@mazy.be>
2022-11-21 18:21:05 +0100

    translations cont

--- scripts/check-translations.py
+++ scripts/check-translations.py
@@ -8,11 +8,42 @@ import pprint
 here = pathlib.Path(__file__).parent.resolve()
 
 reference_file_path = str(here / '../WebApplication/src/locales/en.json')
+language_files_path = glob.glob(str(here / '../WebApplication/src/locales/*.json'))
+source_files_path = glob.glob(str(here / '../WebApplication/src/**/*.vue'), recursive=True)
 
 with open(reference_file_path, 'rb') as f:
     reference_content = json.load(f)
 
-language_files_path = glob.glob(str(here / '../WebApplication/src/locales/*.json'))
+# check if all entries in the default language file are being used
+all_source_files_content = ""
+for source_file in source_files_path:
+    with open(source_file, "rt") as f:
+        all_source_files_content += f.read()
+
+# collect all keys
+all_keys = []
+def add_keys(node, prefix, all_keys):
+    for key in node:
+        if isinstance(node[key], dict):
+            add_keys(node[key], prefix=f"{key}.", all_keys=all_keys)
+        else:
+            all_keys.append(f"{prefix}{key}")
+
+add_keys(reference_content, "", all_keys=all_keys)
+
+for key in all_keys:
+    translations_text = [
+        f'"{key}"',
+        f"'{key}'"
+    ]
+    found = False
+    for translation_text in translations_text:
+        if translation_text in all_source_files_content:
+            found = True
+            break
+
+    if not found:
+        print(f"translation '{key}' does not seem to be used")
 
 
 def compare_json_node(reference, value, prefix, missings = [], excessives = []):
