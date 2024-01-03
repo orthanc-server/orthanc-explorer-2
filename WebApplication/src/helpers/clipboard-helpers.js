d73cea7368378e309f149de8d4d0e48cf11a498a
Alain Mazy <alain@mazy.be>
2024-01-03 12:21:34 +0100

    fix #34: Enable copy button when context is not secure

--- WebApplication/src/helpers/clipboard-helpers.js
+++ WebApplication/src/helpers/clipboard-helpers.js
@@ -1,20 +1,39 @@
 export default {
     copyToClipboard(text, onSuccess, onFailure) {
-        navigator.clipboard.writeText(text).then(
-            () => {
-                if (onSuccess !== undefined) {
-                    onSuccess();
-                } else {
-                    console.log("copied to clipboard: ", text)
+        if (navigator.clipboard) {
+            navigator.clipboard.writeText(text).then(
+                () => {
+                    if (onSuccess !== undefined) {
+                        onSuccess();
+                    } else {
+                        console.log("copied to clipboard: ", text)
+                    }
+                },
+                () => {
+                    if (onFailure !== undefined) {
+                        onFailure();
+                    } else {
+                        console.log("failed to copy to clipboard")
+                    }
                 }
-            },
-            () => {
-                if (onFailure !== undefined) {
-                    onFailure();
-                } else {
-                    console.log("failed to copy to clipboard")
-                }
+            )
+        } else { // this happens, e.g in unsecure contexts (https://discourse.orthanc-server.org/t/copy-icon-oe2-problem/3958/4)
+            const tmp = document.createElement('TEXTAREA');
+            const focus = document.activeElement;
+        
+            tmp.value = text;
+        
+            document.body.appendChild(tmp);
+            tmp.select();
+            document.execCommand('copy'); // note: this is deprecated but we use this code only as a fallback
+            document.body.removeChild(tmp);
+            focus.focus();
+            
+            if (onSuccess !== undefined) {
+                onSuccess();
+            } else {
+                console.log("hopefully copied to clipboard: ", text)
             }
-        )
+        } 
     },
 }
