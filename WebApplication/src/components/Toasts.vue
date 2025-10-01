fe22886d5e28a7869d87113eb596e0ec0f3bc169
Alain Mazy <am@orthanc.team>
2025-10-01 10:28:21 +0200

    fix toast bg color

--- WebApplication/src/components/Toasts.vue
+++ WebApplication/src/components/Toasts.vue
@@ -28,7 +28,7 @@ export default {
         async showToast(toastMessage) {
             this.counter += 1;
             console.log(this.counter, toastMessage);
-            this.toasts.push({title: "", message: toastMessage, id: this.counter});
+            this.toasts.push({ title: "", message: toastMessage, id: this.counter });
 
             await nextTick();
 
@@ -44,11 +44,11 @@ export default {
 <template>
     <div id="toast-container" class="position-fixed bottom-0 end-0 p-3" style="z-index: 11;">
         <div v-for="toast in toasts" :id="'toast-' + toast.id" role="alert" aria-live="polite" aria-atomic="true"
-            class="toast" data-bs-autohide="true">
+            class="toast text-bg-primary" data-bs-autohide="true">
             <div class="d-flex">
-            <div class="toast-body">{{ toast.message }}
-            </div>
-            <button type="button" class="btn-close me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+                <div class="toast-body">{{ toast.message }}
+                </div>
+                <button type="button" class="btn-close me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
             </div>
         </div>
     </div>
