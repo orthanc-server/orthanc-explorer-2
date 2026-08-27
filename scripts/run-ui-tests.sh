7abbb66bb9ac453ccd470476a67e3c15c3cb4c7b
Alain Mazy <am@orthanc.team>
2026-08-27 15:37:25 +0200

    run all tests in CI

--- scripts/run-ui-tests.sh
+++ scripts/run-ui-tests.sh
@@ -37,8 +37,8 @@ pip install -r requirements.txt
 
 playwright install
 
-# python . -v -s --orthanc-url=http://localhost/orthanc --orthanc-mode=orthanc-docker --no-header
-pytest test_auth_service_no_emails.py -v -s --orthanc-mode=orthanc-docker --no-header
+pytest . -v -s --orthanc-mode=orthanc-docker --no-header
+# pytest test_auth_service_no_emails.py -v -s --orthanc-mode=orthanc-docker --no-header
 
 
 echo $(pwd)
